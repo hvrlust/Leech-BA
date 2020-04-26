2aa69412b0091e550c0b8d2767c97a67bb86baef
ibexes <jiaman@live.co.uk>
2020-04-26 18:14:41 +0100

    ordering fix

--- backend/src/discord/command-list.js
+++ backend/src/discord/command-list.js
@@ -490,20 +490,22 @@ const adminCommands = {
                     }
                 }));
                 bot.database.getRankDiscordTags().then(async rows => {
-                    rows.forEach(row => {
+                    for (const row of rows) {
                         if(message.guild.members.filter(member => member.id === row['discord_tag']).size === 0) {
+                            console.log("hi");
                             bot.database.revokeRank(row['discord_tag']).catch(e => console.error(e));
                         }
+                    }
+
+                    bot.database.getRanksWithNoRsnSet().then(async rows => {
+                        await response.edit(rows.length === 0 ? `${message.member}, **nice**! All active ranks have a set RSN!` :
+                            `${message.member}, the following have no RSN set ${rows
+                                .map(row => row['discord_tag'])
+                                .filter(row =>  row !== undefined && row.length > 10)
+                                .map(row =>  `\n - <@${row}>`)
+                                .join(" ")}`)
+                            .catch(() => console.error('unable to edit message'));
                     });
-                });
-                bot.database.getRanksWithNoRsnSet().then(async rows => {
-                    await response.edit(rows.length === 0 ? `${message.member}, **nice**! All active ranks have a set RSN!` :
-                        `${message.member}, the following have no RSN set ${rows
-                            .map(row => row['discord_tag'])
-                            .filter(row =>  row !== undefined && row.length > 10)
-                            .map(row =>  `\n - <@${row}>`)
-                            .join(" ")}`)
-                        .catch(() => console.error('unable to edit message'));
                 });
             });
         }
