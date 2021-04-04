8351699bd803b19aed3306a18941036a8a673e1c
ibexes <jiaman@live.co.uk>
2021-04-04 10:25:05 +0100

    https

---
+++ backend/src/discord/commands/admin/editcommand.js
@@ -0,0 +1,69 @@
+const {getRoleId} = require("../utils");
+const {ADMIN_PREFIX} = require("../utils");
+
+function commandAlreadyExists(command, commands) {
+    return commands.has(command);
+}
+
+function areRealRoles(member, roles) {
+    const t = roles.filter(role => getRoleId(member, role) !== null);
+    return t.length > 0;
+}
+
+function extractArgs(word, args) {
+    const parsedArgs = [];
+    const index = args.indexOf(word);
+    const params = module.exports.parameters.map(param => param.split(' ')[0]);
+    if(index === -1) return parsedArgs;
+    for(let i=index+1; i<args.length; i++){
+        if(params.includes(args[i])){
+            return parsedArgs;
+        }
+        parsedArgs.push(args[i]);
+    }
+    return parsedArgs;
+}
+
+module.exports = {
+    name: 'editcommand',
+    description: 'edits an existing custom copypasta command',
+    help: '```asciidoc\nIn no particular order \n' +
+        '-name :: the name of the command \n' +
+        '-response [optional] :: bot\'s response when calling the command \n' +
+        '-description [optional] :: what shows up as info when calling /commands \n' +
+        '-roles [optional] :: who can call this command \n' +
+        '-deleteafter [optional] :: if the caller message is deleted after posting copypasta' +
+        '\n\nExample usages: \n'+ADMIN_PREFIX+'editcommand -name time -deleteafter -response the time is currently -description ' +
+        'replies with "the time is currently" \n' +
+        ''+ADMIN_PREFIX+'addcommand -name ranks -response The ranks are the following... -description responds with copypasta ' +
+        '-roles ranks customers```',
+    parameters: ["-name name", "-response response", "-role role", "-description description", "-deleteafter"],
+    permittedRoles: ["Server admin"],
+    execute: async (bot, message, params) => {
+        const name = extractArgs("-name", params.args);
+        const role = extractArgs("-role", params.args);
+        const response = extractArgs("-response", params.args);
+        const description = extractArgs("-description", params.args);
+
+        if(name.length !== 1){
+            await message.reply("that command requires -name argument followed by the name, use `"+
+                ADMIN_PREFIX+"help editcommand` for examples");
+            return;
+        }
+
+        if(!commandAlreadyExists(name[0], bot.commands)) {
+            await message.reply("command doesn't exist!");
+            return;
+        }
+
+        if(role.length > 0 && !areRealRoles(message.member, role)){
+            await message.reply("some roles don\'t exist!");
+            return;
+        }
+
+        const deleteAfter = params.args.includes("-deleteafter");
+        await bot.database.addCommand(name[0], role, response, description, deleteAfter);
+        await bot.loadCommands();
+        await message.reply(`${name} command added`);
+    }
+};
