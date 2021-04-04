0b681798fab9eb1ab810b0494e0268cf408786b4
ibexes <jiaman@live.co.uk>
2021-04-04 10:47:18 +0100

    https

--- backend/src/site/main.js
+++ backend/src/site/main.js
@@ -15,7 +15,7 @@ exports.run = function (database, getQueueChannel) {
 
   // Constants
   const HOST = '0.0.0.0';
-  const URL = "leehcba.site";
+  const URL = "leechba.site"; // obtain this instead of hardcoding... stupid reverse proxy cba
 
   const app = express();
   app.use(compression());
